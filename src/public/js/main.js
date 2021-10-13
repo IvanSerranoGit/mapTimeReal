01958de623ad868eb30d4deb4b5d560971828aea
Iván serrano <ivan@MacBook-Pro-de-Ivan.local>
2021-10-13 09:27:17 -0500

    mapRealTime

---
+++ src/public/js/main.js
@@ -0,0 +1,41 @@
+var map = L.map('map-template').setView([19.4968674, -99.1336646], 10);
+
+const tileURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png' 
+const tileURL2 = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
+
+const tile = L.tileLayer(tileURL2);
+// 250481
+// Socket Io
+const socket = io.connect();
+
+// Marker
+const marker = L.marker([19.4968674, -99.1336646]); // kiev, ukraine
+marker.bindPopup('Hello There!');
+map.addLayer(marker);
+
+// Geolocation
+map.locate({enableHighAccuracy: true})
+map.on('locationfound', (e) => {
+  const coords = [e.latlng.lat, e.latlng.lng];
+  const newMarker = L.marker(coords);
+  newMarker.bindPopup('You are Here!');
+  map.addLayer(newMarker);
+  socket.emit('userCoordinates', e.latlng);
+  console.log(coords)
+});
+
+// socket new User connected
+socket.on('newUserCoordinates', (coords) => {
+  console.log(coords);
+  const userIcon = L.icon({
+    iconUrl: '/img/icon2.png',
+    iconSize: [38, 42],
+  })
+  const newUserMarker = L.marker([coords.lat, coords.lng], {
+    icon: userIcon 
+  });
+  newUserMarker.bindPopup('New User!');
+  map.addLayer(newUserMarker);
+}); 
+
+map.addLayer(tile);
